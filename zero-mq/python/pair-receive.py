c5657b449f9b6c37d3993ff920818e70b684f0ee
Max Otta <max.otta@gmail.com>
2018-11-16 09:11:07 +0100

    Added ZeroMQ example + minor cleanup

---
+++ zero-mq/python/pair-receive.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import zmq
+import random
+import sys
+import time
+
+host = "localhost"
+port = "5556"
+context = zmq.Context()
+socket = context.socket(zmq.PAIR)
+socket.connect("tcp://{0}:{1}".format(host, port))
+
+while True:
+    msg = socket.recv()
+    print(msg)
+    socket.send_string("client message to server1")
+    socket.send_string("client message to server2")
+    time.sleep(1)
