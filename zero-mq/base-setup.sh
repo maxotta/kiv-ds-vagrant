3dba52d7db389d2ba4098918f8d97823892bbdc0
Max Otta <Maxmilian.Otta@aimtecglobal.com>
2018-11-30 00:03:40 +0100

    Removed obsolete provisioning scripts.

--- zero-mq/base-setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-yum -q -y install epel-release
-yum -q -y group install 'Development Tools'
-yum -q -y install mc
-yum -q -y install screen
--- zero-mq/python-setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-yum -q -y install python36
-yum -q -y install python36-devel
-yum -q -y install python34-pip
-yum -q -y install python34-devel
-pip3.4 install --upgrade pip
